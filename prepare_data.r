c938b4fcbb0397c770bfb30ae2e42b23de410c9a
Zhisong He <hezhi@d.ethz.ch@bs-dsvr91.ethz.ch>
2020-04-20 21:21:36 +0200

    vignette part 1 finished

--- prepare_data.r
+++ prepare_data.r
@@ -71,3 +71,80 @@ plot2 <- DimPlot(seurat, reduction = "umap", label = TRUE)
 plot1 + plot2
 dev.off()
 
+ct_markers <- c("MKI67","NES","DCX","FOXG1","DLX2","DLX5","ISL1","SOX6","NKX2.1","EMX1","PAX6","GLI3","EOMES","NEUROD6","OTX2","LHX9","TFAP2A","HOXB2","HOXB5")
+png("images/heatmap_ctmarkers.png", height=400, width=800)
+DoHeatmap(seurat, features = ct_markers) + NoLegend()
+dev.off()
+
+cl_markers <- FindAllMarkers(seurat, only.pos = TRUE, min.pct = 0.25, logfc.threshold = log(1.2))
+library(dplyr)
+cl_markers %>% group_by(cluster) %>% top_n(n = 2, wt = avg_logFC) %>% print(n = 50)
+
+library(presto)
+cl_markers_presto <- wilcoxauc(seurat)
+cl_markers_presto %>% group_by(group) %>% top_n(n = 2, wt = logFC) %>% print(n = 40, width=Inf)
+
+top10_cl_markers <- cl_markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_logFC)
+png("images/heatmap_clmarkers.png", height=1000, width=1200)
+DoHeatmap(seurat, features = top10_cl_markers$gene) + NoLegend()
+dev.off()
+
+png("images/featureplot_vlnplot_examples.png", height=400, width=800)
+plot1 <- FeaturePlot(seurat, c("NEUROD2","NEUROD6"), ncol = 1)
+plot2 <- VlnPlot(seurat, features = c("NEUROD2","NEUROD6"), pt.size = 0)
+plot1 + plot2 + plot_layout(widths = c(1, 2))
+dev.off()
+
+library(voxhunt)
+load_aba_data('/links/groups/treutlein/PUBLIC_DATA/tools/voxhunt_rds')
+genes_use <- variable_genes('E13', 300)$gene
+vox_map <- voxel_map(seurat, genes_use=genes_use)
+png("images/voxhunt.png", height=500, width=500)
+plot_map(vox_map)
+dev.off()
+
+new_ident <- setNames(c("Dorsal telen. NPC","Midbrain-hindbrain boundary neuron","Dorsal telen. neuron","Dien. and midbrain excitatory neuron",
+                        "Ventral telen. neuron","G2M dorsal telen. NPC","Dorsal telen. IP","Dien. and midbrain NPC",
+                        "Dien. and midbrain IP and excitatory early neuron","G2M Dien. and midbrain NPC","G2M dorsal telen. NPC","Dien. and midbrain inhibitory neuron",
+                        "Dien. and midbrain IP and early inhibitory neuron","Ventral telen. neuron","Unknown 1","Unknown 2"),
+                      levels(seurat))
+seurat <- RenameIdents(seurat, new_ident)
+png("images/umap_annot.png", height=350, width=400)
+DimPlot(seurat, reduction = "umap", label = TRUE) + NoLegend()
+dev.off()
+
+seurat_dorsal <- subset(seurat, subset = RNA_snn_res.1 %in% c(0,2,5,6,10))
+seurat_dorsal <- FindVariableFeatures(seurat_dorsal, nfeatures = 2000)
+VariableFeatures(seurat) <- setdiff(VariableFeatures(seurat), unlist(cc.genes))
+seurat_dorsal <- CellCycleScoring(seurat_dorsal, s.features = cc.genes$s.genes, g2m.features = cc.genes$g2m.genes, set.ident = TRUE)
+seurat_dorsal <- ScaleData(seurat_dorsal)
+seurat_dorsal <- RunPCA(seurat_dorsal) %>% RunUMAP(dims = 1:20)
+png("images/featureplot_examples_dorsal.png", height=300, width=1500)
+FeaturePlot(seurat_dorsal, c("MKI67","GLI3","EOMES","NEUROD6"), ncol = 4)
+dev.off()
+seurat_dorsal <- ScaleData(seurat_dorsal, vars.to.regress = c("S.Score", "G2M.Score"))
+seurat_dorsal <- RunPCA(seurat_dorsal) %>% RunUMAP(dims = 1:20)
+png("images/featureplot_examples_dorsal_cc.png", height=300, width=1500)
+FeaturePlot(seurat_dorsal, c("MKI67","GLI3","EOMES","NEUROD6"), ncol = 4)
+dev.off()
+
+library(destiny)
+dm <- DiffusionMap(Embeddings(seurat_dorsal, "pca")[,1:20])
+dpt <- DPT(dm)
+seurat_dorsal$dpt <- rank(dpt$dpt)
+png("images/dpt_featureplots_dorsal.png", height=300, width=1500)
+FeaturePlot(seurat_dorsal, c("dpt","GLI3","EOMES","NEUROD6"), ncol=4)
+dev.off()
+
+png("images/dpt_scatterplots_dorsal.png", height=300, width=900)
+plot1 <- qplot(seurat_dorsal$dpt, as.numeric(seurat_dorsal@assays$RNA@data["GLI3",]), xlab="Dpt", ylab="Expression", main="GLI3") + geom_smooth(se = FALSE, method = "loess") + theme_bw()
+plot2 <- qplot(seurat_dorsal$dpt, as.numeric(seurat_dorsal@assays$RNA@data["EOMES",]), xlab="Dpt", ylab="Expression", main="EOMES") + geom_smooth(se = FALSE, method = "loess") + theme_bw()
+plot3 <- qplot(seurat_dorsal$dpt, as.numeric(seurat_dorsal@assays$RNA@data["NEUROD6",]), xlab="Dpt", ylab="Expression", main="NEUROD6") + geom_smooth(se = FALSE, method = "loess") + theme_bw()
+plot1 + plot2 + plot3
+dev.off()
+
+save(seurat, seurat_dorsal, file="../scRNAseq_analysis_vignette_seurat/DS1_seurat_objs.rdata")
+saveRDS(seurat, file="../scRNAseq_analysis_vignette_seurat/DS1_seurat_obj.rds")
+saveRDS(seurat_dorsal, file="../scRNAseq_analysis_vignette_seurat/DS1_seurat_obj_dorsal.rds")
+
+
